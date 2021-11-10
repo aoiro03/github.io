ddbb9c86f32a210791f68c74deab2b6deccf596f
aoiro03 <94065419+aoiro03@users.noreply.github.com>
2021-11-11 01:50:15 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-document.write("hello,world!")
+IMG['Title']=new Image()
+IMG['Title'].src='img/Ew_S-hTVoAE0Xlr.png'
+
+    ctx.clearRect(0, 0, WIDTH, HEIGHT)
+    ctx.fillStyle = 'black'
+    ctx.fillRect(0, 0, WIDTH, HEIGHT)
+    ctx.drawImage(IMG['Title'],0,0)
+    ctx.fillStyle = 'white'
+    ctx.fillText('Press Enter to start', WIDTH / 2, HEIGHT /2 +50)
+    
